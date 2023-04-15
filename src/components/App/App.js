4074bf2642c7a9d48caf8b7b7a20a385674da081
Tuyen Error <caotuyenks1605@gmail.com>
2023-04-15 11:12:28 +0700

    eshopper

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,26 @@
-import Sp from '../SanPham/Sp';
-import React, { Component } from 'react';
-
-
+import logo from './logo.svg';
+import './App.css';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Slider from '../Content/Slider/Slider';
+import LeftSidebar from '../Content/LeftSidebar/LeftSidebar';
+import RightContext from '../Content/RightContent/RightContent';
 function App() {
   return (
-    <div class='App'>
-
-      <Sp tieude="Sản phẩm 1" linkanh="https://dottorprimo.b-cdn.net/wp-content/uploads/2022/03/sanpham-3.png"></Sp>
-      <Sp tieude="Sản phẩm 2" linkanh="https://dottorprimo.b-cdn.net/wp-content/uploads/2022/03/sanpham-3.png"></Sp>
-      <Sp tieude="Sản phẩm 3" linkanh="https://dottorprimo.b-cdn.net/wp-content/uploads/2022/03/sanpham-3.png"></Sp>
-      <Sp tieude="Sản phẩm 4" linkanh="https://dottorprimo.b-cdn.net/wp-content/uploads/2022/03/sanpham-3.png"></Sp>
-      <Sp tieude="Sản phẩm 5" linkanh="https://dottorprimo.b-cdn.net/wp-content/uploads/2022/03/sanpham-3.png"></Sp>
-      <Sp tieude="Sản phẩm 5" linkanh="https://dottorprimo.b-cdn.net/wp-content/uploads/2022/03/sanpham-3.png"></Sp>
-    </div>
-  )
-  
+    <body>
+      <Header></Header>
+      <Slider></Slider>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-3">
+            <LeftSidebar></LeftSidebar>
+          </div>
+          <RightContext></RightContext>
+        </div>
+      </div>
+      <Footer></Footer>
+    </body>
+  );
 }
 
 export default App;
